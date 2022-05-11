92aba8510bdf9a681e4de6c64f0aa8ebf5c50b8f
ridvancakirtr <ridvancakirtr@gmail.com>
2022-05-11 08:56:24 +0300

    build

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ const app = express()
 
 app.use(express.json());
 
-app.use(cors());
-
-//app.use(cors({ origin: ["https://atms-frontend.herokuapp.com"], credentials: true }));
-
+app.use(cors({ origin: ["https://atms-frontend.herokuapp.com"], credentials: true }));
 if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'));
 }
